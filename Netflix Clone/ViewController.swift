aaf7294d5c00b041bf3411ea8b700ca90b8cc074
Abhinay Pratap <abhinaypratap@icloud.com>
2022-07-24 11:27:09 +0530

    Create Project

---
+++ Netflix Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netflix Clone
+//
+//  Created by Abhinay Pratap on 24/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
